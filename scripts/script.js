2b3443c12667cf4cb124c043d1475232a770c3e6
Anderson <62722855+anderdot@users.noreply.github.com>
2024-01-06 21:19:35 -0300

    feat: get dynamic background color for nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burgerButton');
     const navList = document.querySelector('.nav-list');
+    const sectionContainer = document.querySelector('.sections');
+    const sections = sectionContainer.querySelectorAll('section');
 
-    burgerMenu.addEventListener('click', function () {
+    burgerMenu.addEventListener('click', toggleMenu);
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', scrollToSection);
+    });
+
+    function toggleMenu() {
         navList.classList.toggle('show');
         toggleBurgerIcon();
-    });
+    }
 
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-
-            navList.classList.remove('show');
-            toggleBurgerIcon();
-        });
-    });
+    function scrollToSection(e) {
+        e.preventDefault();
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+        navList.classList.remove('show');
+        toggleBurgerIcon();
+    }
 
     function toggleBurgerIcon() {
         burgerMenu.classList.toggle('close');
         burgerMenu.innerHTML = burgerMenu.classList.contains('close') ? '&#10005;' : '&#9776;';
     }
+
+    function getBackgroundColor() {
+        sections.forEach(function (section) {
+            var rect = section.getBoundingClientRect();
+
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                var backgroundColor = window.getComputedStyle(section).backgroundColor;
+                document.documentElement.style.setProperty('--bg-active', backgroundColor);
+                return;
+            }
+        });
+    }
+
+    sectionContainer.addEventListener('scroll', getBackgroundColor);
+    window.addEventListener('load', getBackgroundColor);
 });
