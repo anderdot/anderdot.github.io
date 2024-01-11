11bc594ab74fb19598e06eab8cce355a86feec7b
Anderson <62722855+anderdot@users.noreply.github.com>
2024-01-10 22:27:31 -0300

    fix: Lag background color

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burgerButton');
     const navList = document.querySelector('.nav-list');
-    const sectionContainer = document.querySelector('.sections');
-    const sections = sectionContainer.querySelectorAll('section');
 
     burgerMenu.addEventListener('click', toggleMenu);
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -26,22 +24,4 @@ document.addEventListener('DOMContentLoaded', function () {
         burgerMenu.classList.toggle('close');
         burgerMenu.innerHTML = burgerMenu.classList.contains('close') ? '&#10005;' : '&#9776;';
     }
-
-    function getBackgroundColor() {
-        sections.forEach(function (section) {
-            var rect = section.getBoundingClientRect();
-
-            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-                var backgroundColor = window.getComputedStyle(section).backgroundColor;
-                document.documentElement.style.setProperty('--bg-active', backgroundColor);
-                return;
-            }
-        });
-    }
-
-    sectionContainer.addEventListener('scroll', getBackgroundColor);
-    window.addEventListener('scroll', getBackgroundColor);
-    window.addEventListener('load', getBackgroundColor);
-    window.addEventListener('resize', getBackgroundColor);
-    window.addEventListener('orientationchange', getBackgroundColor);
 });
