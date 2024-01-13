ad2fa39a76099a2fbf2a623790eab54f7df6e7d9
Anderson <62722855+anderdot@users.noreply.github.com>
2024-01-13 19:22:53 -0300

    feat: heiglight current section

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burgerButton');
     const navList = document.querySelector('.nav-list');
+    const sectionContainer = document.querySelector('.sections');
+    const sections = sectionContainer.querySelectorAll('section');
+    const navItems = document.querySelectorAll('.nav-list a');
 
     burgerMenu.addEventListener('click', function () {
         toggleMenu();
@@ -26,8 +29,6 @@ document.addEventListener('DOMContentLoaded', function () {
         burgerMenu.innerHTML = burgerMenu.classList.contains('close') ? '&#10005;' : '&#9776;';
     }
 
-    window.addEventListener('resize', updateStyles);
-
     function updateStyles() {
         const scrollbarStyle = document.getElementById('scrollbarStyle');
         const shouldRemoveStyle = window.innerWidth < 767;
@@ -35,5 +36,29 @@ document.addEventListener('DOMContentLoaded', function () {
         scrollbarStyle.innerHTML = shouldRemoveStyle ? '' : '::-webkit-scrollbar { width: 8px; }';
     }
 
+    function highlightCurrentSection() {
+        sections.forEach(function (section) {
+            var rect = section.getBoundingClientRect();
+            if (rect.bottom > 0 && rect.top < window.innerHeight) {
+                navItems.forEach(function (navItem) {
+                    navItem.classList.remove('active');
+                });
+
+                navItems.forEach(function (navItem) {
+                    if (navItem.getAttribute('href') === '#' + section.id) {
+                        navItem.classList.add('active');
+                    } else {
+                        navItem.classList.remove('active');
+                    }
+                });
+            }
+        });
+    }
+
     updateStyles();
+    sectionContainer.addEventListener('scroll', highlightCurrentSection);
+    window.addEventListener('load', highlightCurrentSection);
+    window.addEventListener('resize', updateStyles);
+    window.addEventListener('resize', highlightCurrentSection);
+    window.addEventListener('orientationchange', highlightCurrentSection);
 });
